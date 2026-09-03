9a48ef0fbba336d09042655d06d27a897a61b217
omarsalemj <omarsalemj77@gmail.com>
2026-09-03 08:46:51 +0300

    vvsf

--- database/dbConnction.js
+++ database/dbConnction.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
-// Database connection function
-// This function connects to the MongoDB database using Mongoose.
 const dbConnection = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/e-commerce");
-        console.log("Database connection successful");
-    } catch (error) {
-        console.error("Database connection error:", error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 };
 
 export default dbConnection;
